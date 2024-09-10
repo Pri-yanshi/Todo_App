fafb734052e39130ac13d2335d8db914de890b96
Pri-yanshi <priyanshimaurya008@gmail.com>
2024-09-10 13:25:31 +0530

    set path to connect dist

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express= require('express');
+const path= require('path')
 
 require('dotenv').config();
 // const cors=require('cors');
@@ -8,12 +9,18 @@ require('./db');
 const PORT= process.env.PORT||3000;
 const app = express();
 // app.use(cors());
+app.use(express.static(path.join(__dirname,'frontend/dist')));
+
+
 app.use(express.json());
+
+
 app.get('/',(req,res)=>{
     res.send('hello');
 })
 
 app.use(todoroute)
+app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'frontend/dist/index.html')));
 app.listen(PORT,()=>{
     console.log(`server listening on: ${PORT}`)
 });
