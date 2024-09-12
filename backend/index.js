0caf021f74d8969d17ac14eb340aa6c73c9bec62
Pri-yanshi <priyanshimaurya008@gmail.com>
2024-09-12 21:44:31 +0530

    port setup

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,6 @@ app.get('/',(req,res)=>{
 
 app.use(todoroute)
 app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'frontend/dist/index.html')));
-app.listen(PORT,()=>{
+app.listen(PORT, '0.0.0.0',()=>{
     console.log(`server listening on: ${PORT}`)
 });
