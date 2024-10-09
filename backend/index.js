598bf18776bd683839a335cc521839f6949b349e
Pri-yanshi <priyanshimaurya008@gmail.com>
2024-10-09 22:23:40 +0530

    changes in cors

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const express= require('express');
 
 require('dotenv').config();
-// const cors=require('cors');
+const cors=require('cors');
 const todoroute =require('./route/todo_route')
 require('./db');
 
 const PORT= process.env.PORT||4000;
 const app = express();
-// app.use(cors());
+app.use(cors({
+    origin:  'https://todo-app-1-6qy9.onrender.com'
+}));
 
 app.use(express.json());
 
@@ -16,7 +18,7 @@ app.get('/',(req,res)=>{
     res.send('hello');
 })
 
-app.use(todoroute)
+app.use('/api',todoroute)
 
 app.listen(PORT,()=>{
     console.log(`server listening on: ${PORT}`)
