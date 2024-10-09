52b140e03cf0193eb21a103b05bef55cbf86e56a
Pri-yanshi <priyanshimaurya008@gmail.com>
2024-10-09 20:38:23 +0530

    add backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://0.0.0.0:3000', // Express backend
+        target: 'https://todo-app-osou.onrender.com', // Express backend
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // Remove '/api' prefix from the path
       },
